0c6746bf0fd16c8313d5bcc2b29eb42ad086322e
Przemek <przemyslaw.dziadek@gmail.com>
2017-04-07 11:24:27 +0200

    add less classes declarations and test | add README information about purpose and class names

--- README.md
+++ README.md
@@ -15,8 +15,83 @@ Browsers Support:
 
 This classes supports old browsers flexbox syntax.
 
+The main purpose of creating this repository was to test sass and less mixins repositiories:
+- sass: https://github.com/Wizzaro/sass-flexbox-mixins
+- less: https://github.com/Wizzaro/less-flexbox-mixins
+
 ## Install
 `brew install wizzaro-flexbox-classes`
+
+## Usage classes
+### Classes for the Parent Element
+#### Display
+```
+display-flex
+display-inline-flex
+```
+#### Flex direction
+```
+flex-direction-row
+flex-direction-row-reverse
+flex-direction-column
+flex-direction-column-reverse
+```
+#### Flex wrap
+```
+flex-wrap-nowrap
+flex-wrap-wrap
+flex-wrap-wrap-reverse
+```
+#### Flex flow
+```
+flex-flow-row-nowrap
+flex-flow-row-wrap
+flex-flow-row-wrap-reverse
+flex-flow-row-reverse-nowrap
+flex-flow-row-reverse-wrap
+flex-flow-row-reverse-wrap-reverse
+flex-flow-column-nowrap
+flex-flow-column-wrap
+flex-flow-column-wrap-reverse
+flex-flow-column-reverse-nowrap
+flex-flow-column-reverse-wrap
+flex-flow-column-reverse-wrap-reverse
+```
+#### Justify content
+```
+justify-content-flex-start
+justify-content-flex-end
+justify-content-center
+justify-content-space-between
+justify-content-space-around
+```
+#### Align items
+```
+align-items-flex-start
+align-items-flex-end
+align-items-center
+align-items-baseline
+align-items-stretch
+```
+#### Align content
+```
+align-content-flex-start
+align-content-flex-end
+align-content-center
+align-content-space-between
+align-content-space-around
+align-content-stretch
+```
+### Classes for the Item Element
+#### Align self
+```
+align-self-auto
+align-self-flex-start
+align-self-flex-end
+align-self-center
+align-self-baseline
+align-self-stretch
+```
 
 ## Licence
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,37 @@
 var gulp = require('gulp');
 var gulpSass = require('gulp-sass');
+var gulpLess = require('gulp-less');
 var gulpCssMin = require('gulp-cssmin');
-var gulpStripComments = require('gulp-strip-comments');
+var gulpStripCssComments = require('gulp-strip-css-comments');
 var gulpRename = require('gulp-rename');
 var gulpHtmlReplace = require('gulp-html-replace');
 
 gulp.task('styles', function() {
-    gulp.src('./src/flexbox-class.scss')
+    gulp.src('./src/sass/flexbox-class.scss')
         .pipe(gulpSass().on('error', gulpSass.logError))
-        .pipe(gulp.dest('./test/dist/css'))
-        .pipe(gulpStripComments())
+        .pipe(gulp.dest('./test/dist/sass/css'))
+        .pipe(gulpStripCssComments())
         .pipe(gulp.dest('./dist'))
         .pipe(gulpCssMin())
         .pipe(gulpRename({suffix: '.min'}))
         .pipe(gulp.dest('./dist'));
+
+    gulp.src('./src/less/flexbox-class.less')
+        .pipe(gulpLess().on('error', console.error.bind(console)))
+        .pipe(gulp.dest('./test/dist/less/css'));
 });
 
 gulp.task('styles-test', function() {
     gulp.src('./test/src/sass/style.scss')
         .pipe(gulpSass().on('error', gulpSass.logError))
-        .pipe(gulp.dest('./test/dist/css'));
+        .pipe(gulp.dest('./test/dist/sass/css'));
+
+    gulp.src('./test/src/less/style.less')
+        .pipe(gulpLess())
+        .pipe(gulp.dest('./test/dist/less/css'));
 });
 
-gulp.task('move-files', function() {
+gulp.task('move-test-files', function() {
     gulp.src('./test/src/**.html')
         .pipe(gulpHtmlReplace({
             items:  '<div class="item">1</div>' +
@@ -40,17 +49,21 @@ gulp.task('move-files', function() {
                             '<div class="item">3</div>' +
                             '<div class="item">4</div>'
         }))
-        .pipe(gulp.dest('./test/dist'));
+        .pipe(gulp.dest('./test/dist/sass'))
+        .pipe(gulp.dest('./test/dist/less'));
 });
 
 gulp.task('watch', function() {
     //style
     gulp.watch('components/**/*.scss', ['styles']);
-    gulp.watch('src/**/*.scss', ['styles']);
+    gulp.watch('components/**/*.less', ['styles']);
+    gulp.watch('src/sass/**/*.scss', ['styles']);
+    gulp.watch('src/less/**/*.less', ['styles']);
     //test style
     gulp.watch('./test/src/sass/**/*.scss', ['styles-test']);
+    gulp.watch('./test/src/less/**/*.less', ['styles-test']);
     //test
-    gulp.watch('./test/src/**/*.html', ['move-files']);
+    gulp.watch('./test/src/**/*.html', ['move-test-files']);
 });
 
-gulp.task('default', ['styles', 'styles-test', 'move-files']);
+gulp.task('default', ['styles', 'styles-test', 'move-test-files']);
